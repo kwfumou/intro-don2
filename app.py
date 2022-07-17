205419ff46d8fb68808baa34d385ecfe8b6bc96f
KM1242 <maeyama.k.aa@gmail.com>
2022-07-18 00:29:24 +0900

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # https://qiita.com/NAKA_G/items/f34738df364af8cbd58e
 
 from flask import Flask, render_template, request, redirect, url_for
-from Intro_don import IntroDon
-instance_introdon = IntroDon()
+# from Intro_don import IntroDon
+# instance_introdon = IntroDon()
 
 
 UPLOAD_FOLDER = '/mp3'
@@ -15,39 +15,18 @@ host='0.0.0.0'
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
-    # return render_template('start.html')
 
 @app.route('/start')
 def start():
-    instance_introdon.reset()
     return render_template('start.html')
 
-@app.route('/problem',methods = ["GET"])
+@app.route('/problem')
 def problem():
-    sent_wav, choices_names_list = instance_introdon.Move2NextProblem()
-    numberProblem = instance_introdon.GetNowProblem()
-    numCorrect = instance_introdon.GetPoint()
-    return render_template('problem.html',choices_names_list=choices_names_list,sent_wav=sent_wav,numberProblem=numberProblem,numCorrect=numCorrect)
-
-@app.route('/problem',methods = ["POST"])
-def answer():
-    print("request.method: ", request.method)
-    test_ans = int(request.form["radio"])
-    print("request.form['send']: ", test_ans)
-    result_flag = instance_introdon.AnswerTheQuestion(test_ans)
-    print("result_flag: ",result_flag)
-    numberProblem = instance_introdon.GetNowProblem()
-    numCorrect = instance_introdon.GetPoint()
-    if result_flag:
-        JudgeWord = "正解！！"
-    else:
-        JudgeWord = "残念！！"
-    return render_template('answer.html',numberProblem=numberProblem,numCorrect=numCorrect,result_flag=result_flag,JudgeWord=JudgeWord)
+    return render_template('problem.html')
 
 @app.route('/sum_point')
 def sum_point():
-    numCorrect = instance_introdon.GetPoint()
-    return render_template('sum_point.html',numCorrect=numCorrect)
+    return render_template('sum_point.html')
 
 if __name__ == "__main__":
     # app.run(debug=True)
